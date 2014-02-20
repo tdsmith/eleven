f4e252f43971960c03d960086c3ff06416eaa287
Tim D. Smith <git@tim-smith.us>
2014-02-20 13:02:33 -0800

    try to fix RTFD build again

--- docs/conf.py
+++ docs/conf.py
@@ -14,12 +14,32 @@
 
 import sys
 import os
-import mock
+
+#import mock
+
+class Mock(object):
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
+
 
 MOCK_MODULES = ['numpy', 'scipy', 'scipy.stats', 'scipy.stats.mstats', 'pandas']
 
 for mod in MOCK_MODULES:
-    sys.modules[mod] = mock.Mock()
+    sys.modules[mod] = Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
